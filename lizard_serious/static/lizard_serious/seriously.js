5647753f7fc97e95fdc2b80371a26840e50c028f
Reinout van Rees <reinout@vanrees.org>
2012-06-20 14:55:54 +0200

    Added a popup with a graph for just one point

--- lizard_serious/static/lizard_serious/seriously.js
+++ lizard_serious/static/lizard_serious/seriously.js
@@ -1,4 +1,15 @@
 // Javascript
+function show_serious_popup() {
+    $("#movable-dialog-content").load('/static_media/lizard_serious/popup.html');
+    $("#movable-dialog").dialog("open");
+    window.setTimeout(reloadGraphs, 150);
+}
+
+
+
+function serious_popup_click_handler(x, y, map) {
+  show_serious_popup();
+}
 
 $(document).ready(function() {
 
@@ -6,4 +17,4 @@ $(document).ready(function() {
         event.preventDefault();
         alert("hoi");
     });
-}
+});

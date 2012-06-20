1ff5ca2b1ee050a3a1e834a1a83134fb32b30b7f
jack <jack.ha@nelen-schuurmans.nl>
2012-06-20 14:32:52 +0200

    Added jquery hide and show

--- lizard_serious/static/lizard_serious/seriously.js
+++ lizard_serious/static/lizard_serious/seriously.js
@@ -3,7 +3,9 @@
 $(document).ready(function() {
     $("a.item-switcher").click(function (event) {
         event.preventDefault();
-        $(".switch-item").css("display", "none");
-        $($(event.currentTarget).data("switch")).css("display", "block");
+        //$(".switch-item").css("display", "none");
+        //$($(event.currentTarget).data("switch")).css("display", "block");
+        $(".switch-item").hide("slow");
+        $($(event.currentTarget).data("switch")).show("slow");
     });
 });
